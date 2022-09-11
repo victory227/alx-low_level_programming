73040b3317b092f9a7bdbd7a2fc67baf1e3e5edc
Victory Ezewele <victorycharis@gmail.com>
2022-09-11 12:36:25 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,21 @@
-#include <stdio.h> /** * main - Entry point * Return: Always O (Success) */ int main(void) 
+lude <stdio.h>
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"); return (O);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }
