493cc66c8f37f80335c35f9bd1d04fbba309d62a
Victory Ezewele <victorycharis@gmail.com>
2022-09-11 09:59:37 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always O (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (O);
+}
